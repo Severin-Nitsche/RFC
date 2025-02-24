4af014c3b499a98c26d4e6a6d9846ef000ec402c
Severin Nitsche <severinnitsche@gmail.com>
2025-02-24 10:54:57 +0100

    feat: Add deepseek-vllm tests
    Added parser for GPT-NER like output format enforcing.

---
+++ src/deepseek.py
@@ -0,0 +1,55 @@
+# Credit to https://michaelwornow.net/2024/01/09/lm-format-enforcer-demo
+
+from vllm import LLM, SamplingParams
+from lmformatenforcer import CharacterLevelParser
+from lmformatenforcer.integrations.vllm import build_vllm_logits_processor, build_vllm_token_enforcer_tokenizer_data
+import json
+import spacy
+
+from ner_parser import NERParser
+import config
+
+nlp = spacy.load('en_core_web_sm')
+
+with open(config.REDDIT_POSTS, "r") as posts_file:
+    posts = json.load(posts_file)
+    prompts = [posts[0]['data']['text'], nlp(posts[0]['data']['text']).sents[0].text]; # TODO: load from json
+
+    deepseek = LLM(model="deepseek-ai/deepseek-v3")
+
+    tokenizer_data = build_vllm_token_enforcer_tokenizer_data(deepseek)
+    sampling_params = SamplingParams() # temperature=?, top_p=?, .max_tokens=?
+
+
+    parsers = [NERParser(prompt, '@@', '##') for prompt in prompts]
+
+    tokenizer_data = build_vllm_token_enforcer_tokenizer_data(deepseek)
+    sampling_params = [
+        SamplingParams(
+            logits_processors = [build_vllm_logits_processor(
+                tokenizer_data, 
+                NERParser(
+                    f'''
+                    I am an excellent linguist. The task is to label person entities in the given text. Below are some examples.
+
+                    Input: I went ahead and decided to contact my son's mother.
+                    Output: @@I## went ahead and decided to contact my @@son##'s @@mother##.
+
+                    Input: My favorite band is on tour and scheduled a show for Valentine's Day in a city near us.
+                    Output: @@My## favorite band is on tour and scheduled a show for Valentine's Day in a city near us.
+
+                    Input: I wasn't there because I was visiting friends in the Netherlands, but my mom and her sister spent time together, and it brought them closer.
+                    Output: @@I## wasn't there because I was visiting @@friends## in the Netherlands, but my @@mom## and her @@sister## spent time together, and it brought them closer.
+
+                    Input: {prompt}
+                    Output: 
+                    ''', '@@', '##')
+            )]
+        ) for prompt in prompts
+    ]
+
+    results = deepseek.generate(prompts, sampling_params=sampling_params)
+    for result in results:
+        print(result.outputs[0].text)
+    with open('tests.json', "w") as out:
+        json.dump(results, out)
---
+++ src/ner_parser.py
@@ -0,0 +1,38 @@
+from lmformatenforcer import CharacterLevelParser, StringParser, SequenceParser, UnionParser
+
+def add_string(parser, string):
+    for char in string:
+        parser = parser.add_character(char)
+    return parser
+
+class NERParser(CharacterLevelParser):
+    """A parser that allows an annotated string."""
+    def __init__(self, string: str, start: str, end: str, open: bool = True):
+        self.string = string
+        self.start = start
+        self.end = end
+        self.open = open
+    def add_character(self, new_character: str) -> CharacterLevelParser:
+        print(f"adding {new_character}")
+        parsers = []
+        if self.string.startswith(new_character):
+            print(f"  {new_character} matches string part")
+            parsers.append(NERParser(self.string[1:], self.start, self.end, self.open))
+        if self.open and self.start.startswith(new_character):
+            print(f"  {new_character} matches {self.start}")
+            parsers.append(SequenceParser([
+                StringParser(self.start[1:]),
+                NERParser(self.string, self.start, self.end, False)
+            ]))
+        if (not self.open) and self.end.startswith(new_character):
+            print(f"  {new_character} matches {self.end}")
+            parsers.append(SequenceParser([
+                StringParser(self.end[1:]),
+                NERParser(self.string, self.start, self.end, True)
+            ]))
+        return UnionParser(parsers)
+    def get_allowed_characters(self) -> str:
+        print((self.string[0] if self.string else "")+(self.start[0] if self.open else self.end[0]))
+        return (self.string[0] if self.string else "")+(self.start[0] if self.open else self.end[0])
+    def can_end(self) -> bool:
+        return (not self.string) and open
