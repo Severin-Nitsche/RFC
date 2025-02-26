a3d2749cad365f1346c03bb7de1b1494d02d3907
Severin Nitsche <severinnitsche@gmail.com>
2025-02-26 14:20:57 +0100

    chore: Remove ner_parser debug logs

--- src/ner_parser.py
+++ src/ner_parser.py
@@ -13,26 +13,26 @@ class NERParser(CharacterLevelParser):
         self.end = end
         self.open = open
     def add_character(self, new_character: str) -> CharacterLevelParser:
-        print(f"adding {new_character}")
+        # print(f"adding '{new_character}'")
         parsers = []
         if self.string.startswith(new_character):
-            print(f"  {new_character} matches string part")
+            # print(f"  '{new_character}' matches string part ({self.string})")
             parsers.append(NERParser(self.string[1:], self.start, self.end, self.open))
         if self.open and self.start.startswith(new_character):
-            print(f"  {new_character} matches {self.start}")
+            # print(f"  '{new_character}' matches {self.start}")
             parsers.append(SequenceParser([
                 StringParser(self.start[1:]),
                 NERParser(self.string, self.start, self.end, False)
             ]))
         if (not self.open) and self.end.startswith(new_character):
-            print(f"  {new_character} matches {self.end}")
+            # print(f"  '{new_character}' matches {self.end}")
             parsers.append(SequenceParser([
                 StringParser(self.end[1:]),
                 NERParser(self.string, self.start, self.end, True)
             ]))
         return UnionParser(parsers)
     def get_allowed_characters(self) -> str:
-        print((self.string[0] if self.string else "")+(self.start[0] if self.open else self.end[0]))
+        # print((self.string[0] if self.string else "")+(self.start[0] if self.open else self.end[0]))
         return (self.string[0] if self.string else "")+(self.start[0] if self.open else self.end[0])
     def can_end(self) -> bool:
         return (not self.string) and open
