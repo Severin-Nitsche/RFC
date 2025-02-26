fd67cd07068fb4a92834472851dfa01f624f5992
Severin Nitsche <severinnitsche@gmail.com>
2025-02-26 14:18:45 +0100

    chore: Change memory requirements to be more sane
    The previously requested 256GB did not seem to get used. Also, the --mem
    flag must not be used on RWTH Cluster according to the HPC wiki.

--- deepseek-slurm.sh
+++ deepseek-slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=deepseek_%J.txt
 #SBATCH --partition=c23g
 #SBATCH --gres=gpu:2
-#SBATCH --mem=256G
+#SBATCH --mem-per-cpu=20G
 
 #SBATCH --time=00:15:00
 #SBATCH --mail-user=severin.nitsche@rwth-aachen.de
