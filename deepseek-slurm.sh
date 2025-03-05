7f93fffef9fdd318f68610177d285bdd835f2f07
Severin Nitsche <severinnitsche@gmail.com>
2025-03-05 12:19:48 +0100

    chore: Adapt job script

--- deepseek-slurm.sh
+++ deepseek-slurm.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
 #SBATCH --job-name=annotate-deepseek
-#SBATCH --output=deepseek_%J.txt
+#SBATCH --output=deepseek_%A_%a.txt
 #SBATCH --partition=c23g
 #SBATCH --gres=gpu:2
 #SBATCH --mem-per-cpu=20G
 
-#SBATCH --time=00:15:00
+#SBATCH --array=1-3%1
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=severin.nitsche@rwth-aachen.de
 #SBATCH --mail-type=ALL
 
@@ -15,4 +16,14 @@ ml load Python/3.12.3
 cd /home/ll464721/RFC
 echo "JOB ID: $SLURM_JOB_ID"
 source venv/deepseek/bin/activate
-python src/deepseek.py
+case ${SLURM_ARRAY_TASK_ID} in
+  "1")
+    python src/deepseek_annotate.py
+    ;;
+  "2")
+    python src/deepseek_verify.py
+    ;;
+  "3")
+    python src/deepseek_classify.py
+    ;;
+esac
